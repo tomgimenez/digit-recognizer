57ac5490b5b7433a11c1ccd39ea7b647efc62ed1
Tomas Gimenez <tomasgimenez86@gmail.com>
2026-08-24 10:38:12 -0300

    add new origin url to cors

--- api/main.py
+++ api/main.py
@@ -12,7 +12,11 @@ app = FastAPI(title='Digit Recognizer API')
 
 app.add_middleware(
    CORSMiddleware,
-   allow_origins= ['http://localhost:5173', 'http://127.0.0.1:5173'],
+   allow_origins= [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://digit-recognizer-frontend.onrender.com'
+    ],
    allow_methods= ['*'],
    allow_headers= ['*']
 )
